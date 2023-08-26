65f24dab8213f4ac1d5cf73549f365a338af3a50
mycro8441 <my444455@gmail.com>
2023-08-26 22:02:48 +0900

    dfgdfg

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,182 +1,91 @@
 import { useState } from "react";
 import styled from "styled-components";
-import {format, addMonths, subMonths} from 'date-fns';
-import {startOfMonth, isSameDay, addDays, parse, endOfMonth, startOfWeek, endOfWeek} from 'date-fns';
+import CalendarBody from "./calendar/CalendarBody";
+
 const Container = styled.div`
     width:100%;
     height:100%;
     overflow-y:auto;
     padding:2em;
+    padding-top:0;
 `
 const Title = styled.div`
   text-align:left;
+  margin-top:1em;
   font-size:${p=>p.theme.fontSizes.title};
   font-weight:${p=>p.theme.fontWeight.title};
 `
-const HeaderContainer = styled.div`
-    width:100%;
-    height:52px;    
-    display:flex;
-    margin-top:1.8em;
-    justify-content: center;
+
+const TimeLineContainer = styled.div`
+    position:relative;
+    margin-top:10px;
+    padding-top:1em;
+    margin-left:16px;
+    &:before {
+    content: '';
+        position: absolute;
+        top: 0;
+        left: -2px; /* Adjust this value as needed */
+        width: 2px; /* Width of the timeline line */
+        height: 100%;
+        background-color: #ccc; /* Color of the timeline line */
+        border-radius: 1px; /* Half of the line width to create rounded corners */
+  }
 `
-const Header = styled.div`
-    width:100%;
-    height:100%;
-    background-color: ${p=>p.theme.colors.blue+"99"};
-    border-top-left-radius: 1em;
-    border-top-right-radius: 1em;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-    padding:0 1.4em;
-    box-shadow: 0 10px 15px 1px #0019FA33;
+const Block = styled.div`
+    position: relative;
+    margin: 0 0 20px 20px;
+    padding: 10px;
+    border-radius: 8px;
+    max-width: 400px;
+
     p {
-        font-size:${p=>p.theme.fontSizes.smallTitle};
-        color:${p=>p.theme.colors.gray1};
-        font-weight:700;
+        margin:0;
+        color:${p=>p.theme.colors.gray4};
+        font-size:${p=>p.theme.fontSizes.smallText};
     }
 `
+const Titlea = styled.div`
+    color: #202126;
+    position: relative;
+    font-size:${p=>p.theme.fontSizes.smallTitle};
+    &::before {
+        content: "";
+        position: absolute;
+        width: 10px;
+        height: 10px;
 
-const DayHeader=styled.div`
-    width:100%;
-    padding:0 2em;
-    display:flex;
-    margin-top:2em;
-    justify-content: space-around;
-    font-size: ${p=>p.theme.fontSizes.smallText};
-    font-weight:${p=>p.theme.fontWeight.smallText};
-    div {
-        width:20px;
+        border-radius: 10px;
+        left: -38px;
+        background-image: ${p=>`linear-gradient(${p.theme.colors.orange+"CC"}, ${p.theme.colors.orange+"CC"}), linear-gradient(to bottom, #FFC187 0%, ${p.theme.colors.orange+"CC"} 100%)`};
+        background-origin: border-box;
+        background-clip: content-box, border-box;
+        border:solid 2px transparent;
+
+        box-shadow: 1px 1px 3px 1px #FF861666;
     }
 `
-const Day = styled.div<{activated:boolean}>`
-    color:${p=>p.activated ? p.theme.colors.blue : p.theme.colors.gray2};
-`
-const RowsContainer = styled.div`
-    width:100%;
-    display:flex;
-    flex-direction: column;
-    gap:5px;
-    margin-top:1em;
-    padding:0 1.5em;
-`
-const DaysContainer = styled.div`
-    display:flex;
-    justify-content: space-around;
-`
-const DayContainer = styled.div`
-    width:40px;
-    height:52px;
-    padding:2px;
-`
-const DayBlock = styled.div`
-    width:100%;
-    height:100%;
-    background-color: #0019FA99;
-    border-radius: 5px;
-    padding:5px;
-    color:white;
-    font-size:${p=>p.theme.fontSizes.smallText};
-    font-weight:${p=>p.theme.fontWeight.smallText};
-`
-const RenderHeader = ({currentMonth, prevMonth, nextMonth}) => {
-
-
-        return (
-        <HeaderContainer>
-            <Header>
-            <svg onClick={()=>prevMonth()} xmlns="http://www.w3.org/2000/svg" width="8" height="14" viewBox="0 0 8 14" fill="none">
-                <path d="M7 13L1 7L7 1" stroke="#F8F8FD" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-            <p>{format(currentMonth, 'yyyy')}년 {format(currentMonth, 'MM')}월</p>
-            <svg onClick={()=>nextMonth()} xmlns="http://www.w3.org/2000/svg" width="8" height="14" viewBox="0 0 8 14" fill="none">
-                <path d="M1 1L7 7L1 13" stroke="#F8F8FD" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-            </Header>
-
-        </HeaderContainer>
-
-        )
-}
-
-const RenderDays = ({selectedDate}) => {
-    const days=[];
-    const date = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-
-    for (let i = 0;i<7;i++) {
-        days.push(
-            <Day activated={false} key={i}>
-                {date[i]}
-            </Day>
-        )
-    }
-
-    return <DayHeader>{days}</DayHeader>
-}
-const RenderCells = ({currentMonth, selectedDate, onDateClick})=> {
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
-    const endDate = endOfWeek(monthEnd);
-
-    const rows=[];
-    let days = [];
-    let day = startDate;
-    let formattedDate = '';
-
-    while(day <= endDate) {
-        for(let i=0;i<7;i++) {
-            formattedDate = format(day, 'd');
-            const cloneDay = day;
-            days.push(
-                <DayContainer>
-                    <DayBlock key={day.getTime()} onClick={()=>onDateClick(cloneDay)}>
-                        <span>
-                            {formattedDate}
-                        </span>
-                    </DayBlock>                    
-                </DayContainer>
-
-            )
-            day = addDays(day, 1);
-        }
-        rows.push(
-            <DaysContainer key={day.getTime()}>
-                {days}
-            </DaysContainer>
-        )
-        days=[];
-    }
-    return <RowsContainer>{rows}</RowsContainer>
-}
 const Calendar = () => {
-    const [currentMonth, setCurrentMonth] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(new Date());
-
-    const prevMonth = () => {
-        setCurrentMonth(subMonths(currentMonth, 1));
-    }
-    const nextMonth = () => {
-        setCurrentMonth(addMonths(currentMonth, 1));
-    }
-    const onDateClick = (day) => {
-        setSelectedDate(day);
-    }
     return <>
         <Container>
             <Title>캘린더</Title>
-            <RenderHeader
-                currentMonth={currentMonth}
-                prevMonth={prevMonth}
-                nextMonth={nextMonth}
-            />
-            <RenderDays selectedDate={selectedDate}/>
-            <RenderCells
-                currentMonth={currentMonth}
-                selectedDate={selectedDate}
-                onDateClick={onDateClick}
-            />
+            <CalendarBody selectedDate={selectedDate} setSelectedDate={setSelectedDate}/>
+            <Title>오늘 일정</Title>
+            <TimeLineContainer>
+                <Block>
+                    <Titlea>
+                        자바스크립트 개발 환경 설정
+                    </Titlea>
+                    <p>오후 1:00 - 오후 2:00</p>
+                </Block>
+                <Block>
+                    <Titlea>
+                        자바스크립트 조건문과 반복문 학습
+                    </Titlea>
+                    <p>오후 4:00 - 오후 5:30</p>
+                </Block>
+            </TimeLineContainer>
         </Container>
     </>
 }
