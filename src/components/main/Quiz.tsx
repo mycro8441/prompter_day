d2ec7a0b521fa3eb785469c41f471a7f49ae38cf
mycro8441 <my444455@gmail.com>
2023-09-03 04:18:11 +0900

    df

--- src/components/main/Quiz.tsx
+++ src/components/main/Quiz.tsx
@@ -185,7 +185,7 @@ const Quiz = () => {
                     </div>
                 </QuizTitle>
                 <AnsButtonContainer>
-                    {data.map((v, i)=><AnsButton status={status[i]} onClick={()=>reveal(i)}>
+                    {data.map((v, i)=><AnsButton key={i} status={status[i]} onClick={()=>reveal(i)}>
                         <p>{String.fromCharCode(97+i) + ") " + v}</p>
                         {status[i] === "answer" && <>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
